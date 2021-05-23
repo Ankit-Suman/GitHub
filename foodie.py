7264c01e476e42dcd1015a7f4149939d1c6b9c9f
Ankit-Suman <ankitsuman318@gmail.com>
2021-05-23 14:29:45 +0530

    new foodie file

--- foodie.py
+++ foodie.py
@@ -1,15 +1,9 @@
 hungry=input("Are you hungry?")
 if hungry=="yes":
-    print("Go and eat pizza ")
-    print("Go and eat Burger ")
-    print("Go and eat Sandwitch")
+    print("Go and eat pizza ,burger , fries")
 
-else:
-    print("I'm Thirsty")
 
-# else :
-#     thirsty=input("Are you thirsty?")
-#     if thirsty=="yes":
-#         print("Go and dring Water")
-#         print("Go and drink Coke")
-#         print("Go and drink Soda")
+else :
+    thirsty=input("Are you thirsty?")
+    if thirsty=="yes":
+        print("Go and dring Water, Juice ,Coke ,Soda")
