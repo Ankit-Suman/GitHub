573bfb7fc5505f3226577fa64be891d02a822301
Ankit-Suman <ankitsuman318@gmail.com>
2021-05-23 14:19:38 +0530

    first version of foodie code

---
+++ foodie.py
@@ -0,0 +1,15 @@
+hungry=input("Are you hungry?")
+if hungry=="yes":
+    print("Go and eat pizza ")
+    print("Go and eat Burger ")
+    print("Go and eat Sandwitch")
+
+else:
+    print("I'm Thirsty")
+
+# else :
+#     thirsty=input("Are you thirsty?")
+#     if thirsty=="yes":
+#         print("Go and dring Water")
+#         print("Go and drink Coke")
+#         print("Go and drink Soda")
